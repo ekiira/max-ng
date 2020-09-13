83d71c3880c6639d1e50a142e55e27aa566db183
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-09-13 14:58:36 +0100

    set up form-validation

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
+
+// style
 import style from './Login.module.css';
+
+// images
 import Spaceship from '../images/undraw_To_the_stars_qhyy.svg';
 import Flag from '../images/undraw_Astronaut_xko2.svg'
-import { useHistory } from 'react-router-dom'
 
 const Login = () => {
   const history = useHistory();
-  const [activebutton, setActiveButton] = useState(false);
+  const [activeButton, setActiveButton] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
   const onChangeHandler = (event, handler) => {
     handler(event.target.value)
@@ -23,90 +28,103 @@ const Login = () => {
     }
   }, [email, password])
 
-  const onFormSubmit = () => {
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if ( password.match(/^(?=.*\d).{8,}$/)) {
+    setEmail('')
+    setPassword('')
     history.push("/dashboard");
+    } else {
+      setError(true)
+    }
   }
 
   return (
-    <div className={`${style.wrapper}`}>
+    <div className={style.wrapper}>
       <div className='row mx-0'>
         <div className={`col-lg-4 pr-0`}>
-    <div className={`row ${style.left}`}>
-      <div className="container">
-        <div className={`col-md-12 h-100 d-flex justify-content-center align-items-center text-center ${style.banner}`}>
-          <div>
-            <div className={` ${style.left_intro}`}>
-                <p className="font-weight-bold text-left">Welcome to Star Wars<br/> the Clone Wars</p>
+          <div className={`row ${style.left}`}>
+            <div className="container">
+              <div className={`col-md-12 h-100 d-flex justify-content-center align-items-center text-center ${style.banner}`}>
+                <div>
+                  <div className={` ${style.left_intro}`}>
+                      <p className="font-weight-bold text-left">Welcome to Star Wars<br/> the Clone Wars</p>
+                  </div>
+                  <div className='pt-5'>
+                    <img src={Spaceship} className='img-fluid'/>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='pt-5'>
-              <img src={Spaceship} className='img-fluid'/>
+            <div className={`${style.black_box}`}>
+              <div className={`${style.black_box}`}></div>
             </div>
-      </div>
           </div>
-       
-        </div>
-        <div className={`${style.black_box}`}>
-
-            <div className={`${style.black_box}`}></div>
-
-          </div>
-        </div>
         </div>
 
         <div className='col-lg-4'>
-        <div className={`row ${style.mid}`}>
-      <div className="container">
-        <div className={`col-md-12  d-flex justify-content-center align-items-center text-center ${style.mid_banner}`}>
-          <div>
-          <div className={`pb-4 ${style.mid_intro}`}>
-                <p className="font-weight-bold text-left">Sign in to continue to<br/> your account.</p>
-            </div>
+          <div className={`row ${style.mid}`}>
+            <div className="container">
+              <div className={`col-md-12  d-flex justify-content-center align-items-center text-center ${style.mid_banner}`}>
+                <div>
+                  <div className={`pb-4 ${style.mid_intro}`}>
+                        <p className="font-weight-bold text-left">Sign in to continue to<br/> your account.</p>
+                    </div>
 
-            <div className={`pt-3 ${style.form}`}>
-             <Form>
-  <Form.Group className='pb-3'>
-    <Form.Label className='text-left d-flex'>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Email address" className=''/>
-    
-  </Form.Group>
+                  <div className={`pt-3 ${style.form}`}>
+                    <Form onSubmit={onFormSubmit}>
+                      <Form.Group controlId="formEmail" className='pb-3'>
+                        <Form.Label className='text-left d-flex'>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Email address" value={email} onChange={(e) => onChangeHandler(e, setEmail)}/>
+                      </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label className='text-left d-flex'>Password</Form.Label>
-    <Form.Control type="password" placeholder="Enter strong password" />
-  </Form.Group>
+                    <Form.Group controlId="formPassword">
+                      <Form.Label className='text-left d-flex'>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Enter strong password" value={password} onChange={(e) => onChangeHandler(e,setPassword)}/> 
+
+                      {error ? (
+                            <Form.Text className="text-muted">
+                            Your passwword must contain at least 8 characters, including a number
+                          </Form.Text>
+                      
+                      ) : null}
+                    </Form.Group>
   
   <div className='pt-3'>
-  <Button variant="dark" type="submit" block > 
-    Sign in
-  </Button>
+    {activeButton? (
+      <Button variant="dark" type="submit" block > 
+      Sign in
+    </Button>
+    ) : (
+<Button variant="dark" type="submit" block disabled> 
+Sign in
+</Button>
+    )}
+  
   </div>
 </Form>
-            
+                  </div>
+                </div>
+              </div>
             </div>
-           
-      </div>
-
           </div>
-        </div>
-    </div>
-  
         </div>
      
         <div className={`col-lg-3 d-none d-lg-block`}>
-    <div className={`row ${style.right}`}>
-      <div className="container">
-        <div className={`col-md-12 h-100  d-flex justify-content-end align-items-start text-center ${style.banner}`}>
-          <div>
-            <div className={`pt-5 ${style.right_intro}`}>
-                <img src={Flag} className='img-fluid'/>
+          <div className={`row ${style.right}`}>
+            <div className="container">
+              <div className={`col-md-12 h-100  d-flex justify-content-end align-items-start text-center ${style.banner}`}>
+                <div>
+                  <div className={`pt-5 ${style.right_intro}`}>
+                      <img src={Flag} className='img-fluid'/>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-</div>
-</div>
   )
 } 
 
